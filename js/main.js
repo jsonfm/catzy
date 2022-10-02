c4ad1f422b1f23f597a34fa164cae09179e960af
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-02 16:20:44 -0500

    Added Favourites section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,54 @@
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=6'
-const catsContainer = document.getElementById('catsContainer');
+const API_KEY = 'live_S2Yfckv7yM9yyxXZnmCdim91QdNltzv4SYcoHM8OaLDDIibwMnv2eXCtKDON9P0b';
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
-const getRandomMichis = async () => {
-    return await fetch(API_URL).then(res => res.json())
+
+const catsContainer = document.getElementById('catsContainer');
+const catsFavContainer = document.getElementById('catsFavContainer');
+
+
+const saveCat = async (id) => {
+    const res = await fetch(`${BASE_URL}/favourites`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': API_KEY,
+        },
+        body: JSON.stringify({
+          image_id: id
+        }),
+    })
+    .then(res => res.json());
+    console.log("res: ", res)
+    getFavourites();
+}
+
+const deleteCat = async(id) => {
+    const res = await fetch(`${BASE_URL}/favourites/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'x-api-key': API_KEY,
+        }
+    })
+    .then(res => res.json());
+    getFavourites();
 }
 
 const renderCatCard = (cat) => {
     const html = `
         <div class="cat-card">
             <img class="cat-image" src="${cat.url}">
-            <div class="button"><i class="fa-regular fa-heart"></i></div>
+            <div class="button" onclick="saveCat('${cat.id}')"><i class="fa-regular fa-heart"></i></div>
+        </div>
+    `  
+    return html; 
+}
+
+const renderFavCatCard = (cat) => {
+    const html = `
+        <div class="cat-card">
+            <img class="cat-image" src="${cat.image.url}">
+            <div class="button" onclick="deleteCat('${cat.id}')"><i class="fa-sharp fa-solid fa-trash"></i></div>
         </div>
     `  
     return html; 
@@ -22,6 +60,21 @@ const renderCatsList = (cats) => {
     catsContainer.innerHTML = html;    
 }
 
-getRandomMichis().then((cats) => {
-    renderCatsList(cats);
-})
+const renderFavoritesList = (cats) => {
+    let html = ``;
+    cats.map((cat) => html += renderFavCatCard(cat))
+    catsFavContainer.innerHTML = html;   
+}
+
+const getFavourites = async() => {
+    const cats = await fetch(`${BASE_URL}/favourites/?api_key=${API_KEY}`).then(res => res.json());
+    renderFavoritesList(cats);
+}
+
+const getRandomMichis = async () => {
+    const cats = await fetch(`${BASE_URL}/images/search?limit=6`).then(res => res.json());
+    renderCatsList(cats)
+ }
+
+getRandomMichis();
+getFavourites();
